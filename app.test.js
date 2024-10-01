e843410d1f6a7df7128a44ffd299c72e8398c364
paweude <paweldunal90@gmail.com>
2024-10-01 13:45:05 +0200

    initial commit

---
+++ app.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const { app, server } = require('./app');
+
+describe('GET /', () => {
+  it('should return "Hello World"', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello World');
+  });
+  afterAll((done) => {
+    server.close(done);
+  });
+});
